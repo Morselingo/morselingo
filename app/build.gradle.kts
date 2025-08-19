e65dc4d1dcbaa84e5c06cea1c92ddb148cbca2a9
Thomas Schleicher <thschleicher@edu.aau.at>
2025-08-19 16:53:44 +0200

    feat: upgrade to API 36 and dependencies for compose navigation and view models

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "at.aau.morselingo"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "at.aau.morselingo"
-        minSdk = 35
-        targetSdk = 35
+        minSdk = 36
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
