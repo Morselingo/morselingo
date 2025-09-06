8c8e3f922b7cc14f85164247b98d15f0ab64f9da
Thomas Schleicher <thscleicher@edu.aau.at>
2025-09-06 12:47:05 +0200

    feat: setup for stats page

---
+++ app/src/main/java/at/aau/morselingo/statistics/StatisticsScreen.kt
@@ -0,0 +1,8 @@
+package at.aau.morselingo.statistics
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StatisticsScreen() {
+
+}
---
+++ app/src/main/java/at/aau/morselingo/statistics/StatisticsScreenViewModel.kt
@@ -0,0 +1,11 @@
+package at.aau.morselingo.statistics
+
+import androidx.lifecycle.ViewModel
+import at.aau.morselingo.data.MorseStatsRepository
+
+class StatisticsScreenViewModel(
+    private val repository: MorseStatsRepository
+) : ViewModel() {
+
+
+}
